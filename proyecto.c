3c3d1abaf2691088f3614152289e6f9972cd3bf0
0HACHE0 <44127842+0HACHE0@users.noreply.github.com>
2018-10-19 14:06:56 -0500

    19101406.c
    001

--- proyecto.c
+++ proyecto.c
@@ -6,45 +6,59 @@
 
 int main(void)
 {
+    float xob=0.2, yob=0.5;                                 //VARIABLES PARA GUARDAR LA FUNCION OBJETIVO
+    unsigned char signoob=1;                          //VARIABLES PARA GUARDAR LA FUNCION OBJETIVO
+    /*
+    printf("\nIntroduce la funcion objetivo:  ");
+    scanf("%f %c %f",&xob,&signoob,&yob);
+    if(signoob==43)                                 //SI EL SIMBOLO ES SUMA (SIGNO VALE 1)
+            signoob=1;
+    else                                            //SI EL SIGNO ES RESTA (SIGNO VALE 2)
+            signoob=2;
+    */
     srand(time(NULL));
-    int restricciones;
-    printf("Numero de restricciones: ");
+    int restricciones=3;
+    /*
+    printf("\nNumero de restricciones: ");
     scanf("%d",&restricciones);                     //PIDE CUANTAS RESTRICCIONES VAS A QUERER
     printf("\n");
-    float x[5][1],y[5][1],res[5][1],xl[5][1],yl[5][1];  //ARREGLOS PARA GUARDAR LA SRESTRICCIONES
-    unsigned char signo[5][1];                          //ARREGLO PARA GUARDAR SIGNO
-    char condicion[5][2];                               //'''''''''''''''''''' CONDICION
-    float valor_x[5][1],valor_y[5][1];              //ARREGLO PARA GUARDAR DIVISION (LIMITES)
+    */
+    float x[5]={1,1,1,0,0},y[5]={6,1,0,0,0},res[5]={20000,6000,4000,0,0},xl[5],yl[5];  //ARREGLOS PARA GUARDAR LAS RESTRICCIONES
+    unsigned char signo[5]={1,1,1,0,0};                          //ARREGLO PARA GUARDAR SIGNO
+    char condicion[5][2]={1,1,1,0,0};                               //'''''''''''''''''''' CONDICION
+    float valor_x[5],valor_y[5];              //ARREGLO PARA GUARDAR DIVISION (LIMITES)
     int j, i=0;
-    float xmax=100000,ymax=100000,xmin=0,ymin=0;    //INICIALIZO MIN EN 0(NEGATIVIDAD) Y MAX EN UN N MUY GRANDE.
+    float xmax=100000,ymax=100000,xmin=0,ymin=0;    //INICIALIZO MIN EN 0(NEGATIVIDAD) Y MAX EN UN N MUY GRANDE
     for(j = 0; j<restricciones; j++){               //ESTE FOR PEDIRA CADA RESTRICCION
+        /*
         printf("Restriccion %d: ",j+1);
-        scanf("%f %c %f %s %f",&x[j][i],&signo[j][i],&y[j][i],&condicion[j],&res[j][i]);
+        scanf("%f %c %f %s %f",&x[j],&signo[j][i],&y[j],&condicion[j],&res[j][i]);
+        */
 
-        if(x[j][i]==0){                             //VALIDACION SI X ES IGUAL A 0
-            valor_x[j][i]=0;
-            //printf("X %.2f ",valor_x[j][i]);
+        if(x[j]==0){                             //VALIDACION SI X ES IGUAL A 0
+            valor_x[j]=0;
+            //printf("X %.2f ",valor_x[j]);
         }
         else{
-            //printf("X %.2f ",x[j][i]);
-            valor_x[j][i] = res[j][i] / x[j][i];    //SI X ES DIF DE 0 HACE LA DIVISIÓN
-            //printf("LIMX %.2f ",valor_x[j][i]);
+            //printf("X %.2f ",x[j]);
+            valor_x[j] = res[j] / x[j];    //SI X ES DIF DE 0 HACE LA DIVISIÓN
+            //printf("LIMX %.2f ",valor_x[j]);
         }
 
-        if(y[j][i]==0){                             //VALIDACION SI Y ES IGUAL A 0
-            valor_y[j][i]=0;
+        if(y[j]==0){                             //VALIDACION SI Y ES IGUAL A 0
+            valor_y[j]=0;
         }
-        else{                                       //SI Y ES DIF DE O HACE LA DIVICION
-            valor_y[j][i] = res[j][i]/y[j][i];
+        else{                                       //SI Y ES DIF DE 0 HACE LA DIVICION
+            valor_y[j] = res[j]/y[j];
         }
 
-        if(signo[j][i]==43){                        //SI EL SIMBOLO ES SUMA (SIGNO VALE 1)
+        if(signo[j]==43){                        //SI EL SIMBOLO ES SUMA (SIGNO VALE 1)
             //printf("suma ");
-            signo[j][i]=1;
+            signo[j]=1;
         }
         else{                                       //SI EL SIGNO ES RESTA (SIGNO VALE 2)
             //printf("resta ");
-            signo[j][i]=2;
+            signo[j]=2;
         }
 
         if(!strcmp(condicion[j],">=")){             //SI LA CONDICION ES >= ( CONDICION VALE 1 )
@@ -55,74 +69,78 @@ int main(void)
             //printf("MENOR igual\n");
             condicion[j][i]=2;
                                                     //ENTONCES
-            if(x[j][i]!=0)                          //SI X DE RESTRICCION ES DIF DE 0
-                xl[j][i]=res[j][i]/x[j][i];         //X LIM ES (RES DE RESTRICCION / X DE RESTRICCION)
+            if(x[j]!=0)                          //SI X DE RESTRICCION ES DIF DE 0
+                xl[j]=res[j]/x[j];         //X LIM ES (RES DE RESTRICCION / X DE RESTRICCION)
             else                                    //SI X DE RESTRICCION ES 0
-                xl[j][i]=xmax;                      //X LIM ES XMAX
+                xl[j]=xmax;                      //X LIM ES XMAX
 
-            if(y[j][i]!=0)                          //SI Y DE RESTRICCION ES DIF DE 0
-                yl[j][i]=res[j][i]/y[j][i];         //Y LIM ES (RES DE RESTRICCION / Y DE RESTRICCION)
+            if(y[j]!=0)                          //SI Y DE RESTRICCION ES DIF DE 0
+                yl[j]=res[j]/y[j];         //Y LIM ES (RES DE RESTRICCION / Y DE RESTRICCION)
             else                                    //SI Y DE RESTRICCION ES 0
-                yl[j][i]=ymax;                      //Y LIM ES YMAX
+                yl[j]=ymax;                      //Y LIM ES YMAX
 
-            if(xl[j][i]<xmax || yl[j][i]<ymax){     //EN CASO DE QUE (X LIM ES MENOR A XMAX) Ó (Y LIM ES MENOR A YMAX)
-                xmax=xl[j][i];                      //XMAX ES IGUAL A X LIM
-                ymax=yl[j][i];                      //YMAX ES IGUAL A Y LIM
+            if(xl[j]<xmax || yl[j]<ymax){     //EN CASO DE QUE (X LIM ES MENOR A XMAX) Ó (Y LIM ES MENOR A YMAX)
+                xmax=xl[j];                      //XMAX ES IGUAL A X LIM
+                ymax=yl[j];                      //YMAX ES IGUAL A Y LIM
             }                                       //DE ESTA MANERA SE ACTUALIZA LOS LIMITES REALES DE LAS CONDICIONES PARA LA GENERACION DE NUMEROS
         }
     }
+    /*
     printf("-----------\n",xmax);
     printf("xmax %.2f\n",xmax);
     printf("xmin %.2f\n",xmin);
     printf("ymax %.2f\n",ymax);
     printf("ymin %.2f\n",ymin);
     printf("-----------\n",xmax);
+    */
 
-    unsigned char bits;
+    unsigned char bits=0;
     int mjx, mjy;
     int limx, limy;
+    /*
     printf("\nINGRESE EL NUMERO DE BITS: ");
     scanf("%d",&bits);
+    */
     limx=(xmax-xmin)*(pow(10,bits));
     limy=(ymax-ymin)*(pow(10,bits));
 
     mjx= ceil(log(limx)/log(2));
     mjy= ceil(log(limy)/log(2));
+    /*
     printf("\n\tPara x %d bits",mjx);
     printf("\n\tPara y %d bits",mjy);
+    */
 
     int numbitx, numbity;
     numbitx = pow(2,mjx)-1;                     //SE NECESITA DISMINUIR 2 EN VEZ DE 1 PARA PONER REALIZAR CAMBIOS EN LOS BITS PARA LOS ALEATORIOS
     numbity = pow(2,mjy)-1;
-    printf("\nmaxbit x: %d  &  maxbit y: %d\n",numbitx,numbity);
-                                                                    //EJEMPLOS CON AND PARA VERIFICAR QUE SE HAGA LA RESTRICCION
-    int ej1=25730, ej2=34769;
-    printf(" %d \n",ej1&numbitx);
-    printf(" %d \n",ej2&numbity);
+    //printf("\nmaxbit x: %d  &  maxbit y: %d\n",numbitx,numbity);
 
-
-    system("pause");
-
-
-    float x1[5][1], y1[5][1], res1[5][1];           //ARREGLOS PARA CREACION DE NUMEROS (AQUI VAMOS A PONER ALEATORIOS)
-    int k;
+    int auxintx=0, auxinty=0;
+    float auxfloatx=0, auxfloaty=0;
     unsigned char valido;
-    for ( j=0 ; j<5 ; j++){
+    int k;
+    int f;
+    float taux=0;
+
+    printf("\nCuantos numeros deseas generar:  ");
+    scanf("%d",&f);
+    printf("\n");
+
+    float tx[f],ty[f],tzj[f],tzj1[f],tzjacum[f],tale[f],tvect[f]; //ARREGLOS PARA CALCULOS DE F1
+
+    float x1[f], y1[f], res1[f];           //ARREGLOS PARA CREACION DE NUMEROS (AQUI VAMOS A PONER ALEATORIOS)
+    for ( j=0 ; j<f ; j++){
         valido=0;
         while(valido!=restricciones){
-
-            printf("Ingrese X & Y:  ");
-            //x1[j][i]=rand();
-            //x1[j][i]=x1[j][i] % numbitx;
-            //y1[j][i]=rand();
-            //y1[j][i]=y1[j][i] % numbity;
-            scanf( "%f %f",&x1[j][i],&y1[j][i] );
+            x1[j]=rand()%numbitx;
+            y1[j]=rand()%numbity;
             for(k=0 ; k<restricciones ; k++){
-                if( signo[k][i]==1 ){
-                    res1[j][i] = x[k][i]*x1[j][i] + y[k][i]*y1[j][i];
-                    if(res1[j][i]>=0){
+                if( signo[k]==1 ){
+                    res1[j] = x[k]*x1[j] + y[k]*y1[j];
+                    if(res1[j]>=0){
                         if(condicion[k][i]==1){
-                            if(res1[j][i]>=res[k][i]){
+                            if(res1[j]>=res[k]){
                                 printf("%c R%d  ",2,k+1);
                                 valido++;
                             }
@@ -130,7 +148,31 @@ int main(void)
                                 printf("X R%d  ",k+1);
                         }
                         else{
-                            if(res1[j][i]<=res[k][i]){
+                            if(res1[j]<=res[k]){
+                                printf("%c R%d  ",2,k+1);
+                                valido++;
+                            }
+                            else
+                                printf("X R%d  ",k+1);
+                        }
+                    }
+                    else{
+                        printf("X R%d  ",k+1);
+                    }
+                }
+                else{
+                    res1[j] = x[k]*x1[j] - y[k]*y1[j];
+                    if(res1[j]>=0){
+                        if(condicion[k][i]==1){
+                            if(res1[j]>=res[k]){
+                                printf("%c R%d  ",2,k+1);
+                                valido++;
+                            }
+                            else
+                                printf("X R%d  ",k+1);
+                        }
+                        else{
+                            if(res1[j]<=res[k]){
                                 printf("%c R%d  ",2,k+1);
                                 valido++;
                             }
@@ -141,52 +183,30 @@ int main(void)
                     else
                         printf("X R%d  ",k+1);
                 }
-                else{
-                    res1[j][i] = x[k][i]*x1[j][i] - y[k][i]*y1[j][i];
-                    if(res1[j][i]>=0){
-                        if(condicion[k][i]==1){
-                            if(res1[j][i]>=res[k][i]){
-                                printf("%c R%d  ",2,k+1);
-                                valido++;
-                            }
-                            else
-                                printf("X R%d  ",k+1);
-                        }
-                        else{
-                            if(res1[j][i]<=res[k][i]){
-                                printf("%c R%d  ",2,k+1);
-                                valido++;
-                            }
-                            else
-                                printf("X R%d  ",k+1);
-                        }
-                    }
-                    else
-                        printf("X R%d",k+1);
-                }
-                //printf(" %.2f ",res1[j][i]);
             }
             if( valido == restricciones){
-                printf("\t%.2f & %.2f es VALIDO\n",x1[j][i],y1[j][i]);
+                printf("\t%.2f & %.2f es VALIDO\n",x1[j],y1[j]);
+                                                                        //ESTA SERA UNA FUNCIÓN
+                tx[j]= xmin + ( x1[j]*( (xmax-xmin)/numbitx) );
+                ty[j]= ymin + ( y1[j]*( (ymax-ymin)/numbity) );
+                if(signoob==1)
+                    tzj[j]=tx[j]+ty[j];
+                else
+                    tzj[j]=tx[j]-ty[j];
+                taux=taux+tzj[j];
+                                                                        //AQUI TERMINARA LA FUNCIÓN
             }
             else{
-                printf("\tINTRODUCE UN NUEVO NUMERO\n",x1[j][i],y1[j][i]);
+                printf("\tINTRODUCE UN NUEVO NUMERO\n",x1[j],y1[j]);
+                valido=0;
             }
-
         }
     }
+    printf("\n");
     int g;
-    for(g=0 ; g<5 ; g++){
-        printf("\t%.2f & %.2f\n",x1[g][i],y1[g][i]);
+    for(g=0 ; g<f ; g++){
+        //printf("\t%d.-   %.2f & %.2f\n",g+1,x1[g][i],y1[g][i]);
+        printf("%.2f | %.2f | %.2f | %.4f\n",tx[g],ty[g],tzj[g],tzj[g]/taux);
     }
-
-    printf("\n000000000000000000000000000000\n");
-    /*
-    if(){
-
-    }
-    */
-
-
     return 0;
 }
